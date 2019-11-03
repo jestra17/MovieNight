abef9dd80471699f4c5a8e928f6b640bba41ff3d
Anna Kovneva <kovnevaa@yahoo.com>
2019-11-03 11:56:46 -0800

    reccomend feature

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm 
-from wtforms import StringField, PasswordField, BooleanField
+from wtforms import StringField, PasswordField, BooleanField   
 from wtforms.validators import InputRequired, Email, Length
 from flask_sqlalchemy  import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from flask_sqlalchemy import SQLAlchemy
-
 
 app = Flask(__name__)
 
@@ -42,6 +41,7 @@ class RegisterForm(FlaskForm):
     email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
     username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
     password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+
 
 @app.route("/")
 def home():
@@ -82,10 +82,10 @@ def signup():
 
 
 @app.route("/recommend")
-@login_required
+#@login_required
 def recommend():
-    return render_template("recommend.html",name=current_user.username)
-
+    return render_template("recommend.html")
+#name=current_user.username goes in return for recc commented out for editing purpose
 @app.route('/logout')
 @login_required
 def logout():
